ce57db4e88ad1664a4adb3ab800dda5b971146b8
cardel87 <cardel87@cb9928a3-e18e-1bc1-f28b-18457e860426>
2010-10-04 23:51:01 +0000

    Avance Cardel

---
+++ ProyectoKDD/src/Persistencia/FachadaBDWeka.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Persistencia;
+import weka.experiment.DatabaseUtils;
+/**
+ *
+ * @author carands
+ */
+public class FachadaBDWeka {
+
+    weka.experiment.
+}
