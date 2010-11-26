550d956db47242efac1fd811a3f638575fadfb9f
cardel87 <cardel87@cb9928a3-e18e-1bc1-f28b-18457e860426>
2010-11-26 05:27:28 +0000

    Mi avance.

--- trunk/ProyectoKDD/src/Papelera/FachadaBD.java
+++ trunk/ProyectoKDD/src/Papelera/FachadaBD.java
@@ -6,7 +6,9 @@ package Papelera;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import weka.experiment.InstanceQuery;
 
 /**
  *
@@ -18,20 +20,20 @@ public class FachadaBD {
     private String usuario;
     private String contrasena;
     private Connection conexion;
+    private InstanceQuery query;
 
-    public FachadaBD()
-    {
-        url="jdbc:mysql://localhost:3306/colmovil";
-        usuario= "colmovil";
-        contrasena= "colmovil";
+    public FachadaBD() {
+        url = "jdbc:mysql://localhost:3306/colmovil";
+        usuario = "colmovil";
+        contrasena = "colmovil";
     }
 
     public Connection abrirConexion() {
         if (conexion == null) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                conexion = DriverManager.getConnection(url,usuario, contrasena);
-                System.out.println( "Conexion exitosa");
+                conexion = DriverManager.getConnection(url, usuario, contrasena);
+                System.out.println("Conexion exitosa");
             } catch (Exception exception) {
                 System.out.println(exception.toString());
             }
@@ -48,10 +50,20 @@ public class FachadaBD {
         }
     }
 
+    public ResultSet realizarConsultaABaseDeDatos(String consulta) throws Exception {
+        query = new InstanceQuery();
+        query.setDatabaseURL("jdbc:mysql://localhost:3306/colmovil");
+        query.setUsername("colmovil");
+        query.setPassword("colmovil");
 
-    public static void main(String[] args)
-    {
-        FachadaBD objFachadaBD= new FachadaBD();
+        query.connectToDatabase();
+        query.execute(consulta);
+        ResultSet salida = query.getResultSet();
+        return salida;
+    }
+
+    public static void main(String[] args) {
+        FachadaBD objFachadaBD = new FachadaBD();
         objFachadaBD.abrirConexion();
         objFachadaBD.cerrarConexion();
 
